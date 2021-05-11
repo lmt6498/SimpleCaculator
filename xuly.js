9a76bd722c3434deac0abc5c169b44094fefaa46
lmt6498 <lmt6498@users.noreply.github.com>
2021-05-11 15:13:45 +0700

    Nop bai tap

---
+++ xuly.js
@@ -0,0 +1,59 @@
+function num1() {
+    caculator.display.value += 1;
+}
+
+function num2() {
+    caculator.display.value += 2;
+}
+
+function num3() {
+    caculator.display.value += 3;
+}
+
+function numadd() {
+    caculator.display.value += '+';
+}
+
+function num4() {
+    caculator.display.value += 4;
+}
+
+function num5() {
+    caculator.display.value += 5;
+}
+
+function num6() {
+    caculator.display.value += 6;
+}
+
+function numsub() {
+    caculator.display.value += '-';
+}
+
+function num7() {
+    caculator.display.value += 7;
+}
+
+function num8() {
+    caculator.display.value += 8;
+}
+
+function num9() {
+    caculator.display.value += 9;
+}
+
+function nummul() {
+    caculator.display.value += '*';
+}
+
+function num0() {
+    caculator.display.value += 0;
+}
+
+function numkq() {
+    caculator.display.value = eval(caculator.display.value);
+}
+
+function numdev() {
+    caculator.display.value += '/';
+}
